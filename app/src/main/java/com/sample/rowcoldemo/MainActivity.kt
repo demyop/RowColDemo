e8add3fc04a3fd3b4c71268a97854dfe51ebf4c4
HerenKhar <efednova@mail.ru>
2025-10-29 20:41:21 +0700

    Composable Row

--- app/src/main/java/com/sample/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/sample/rowcoldemo/MainActivity.kt
@@ -7,41 +7,54 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.sample.rowcoldemo.ui.theme.RowColDemoTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.border
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RowColDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            StringsDemoTheme()
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RowColDemoTheme {
-        Greeting("Android")
+    @Composable
+    fun TextCell(text: String, modifier: Modifier = Modifier) {
+        val cellModifier = modifier
+            .padding(4.dp)
+            .size(100.dp, 100.dp)
+            .border(width = 4.dp, color = Color.Black)
+        Text(text = text, cellModifier.then(modifier),
+            fontSize = 80.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center)
     }
-}
+    @Composable
+    fun MainScreen(modifier: Modifier = Modifier) {
+        Row(modifier) {
+            TextCell("1")
+            TextCell("2")
+            TextCell("3")
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun StringsDemoTheme() {
+            MainScreen()
+        }
+    }
